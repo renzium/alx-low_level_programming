9a6346805a04cc1fdef3bc36dabe8802090a1e8e
Lawrence Ughonu <lawrenceughonu@gmail.com>
2022-09-14 14:42:48 +0100

    2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase
+ *
+ *
+ * return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 1; 
+	char c;
+	while (i != 11)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{ 
+			putchar(c);
+		}
+		putchar('\n');
+
+		i++;
+	}
+}
